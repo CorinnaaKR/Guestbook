f0fb7137b0f83baa6d3d7c630e7f539221eb33e6
Corinna <x.corinna.x@hotmail.co.uk>
2025-09-05 15:13:41 +0100

    created the endpoint and tested it, then started on querying the data from the database to send back to the server, then client. Updated scratch with more demo code, updated reflections

--- server/scratch.js
+++ server/scratch.js
@@ -1,7 +1,7 @@
 //MANNY DEMO FOR USER INPUT-POST ROUTES
 
 //TODO: I want to READ all the data from the staff table
-// http://localhost:8080/staff --> endpoint, params
+// http://localhost:8080/guests --> endpoint, params
 app.get("/getDBData", async function (req, res) {
   //we need to query our database here
   const query = await db.query(`SELECT * FROM guestFeedback;`);
@@ -9,6 +9,117 @@ app.get("/getDBData", async function (req, res) {
   //parse data into JSON and wrangle data
   res.json(query.rows);
 });
+
+
+
+//find wrangled data demo
+async function retrieveComments() {
+  const response = await fetch(
+    "https://localhost:8080/guests"
+  );
+
+  console.log(response);
+
+  const guestsComments = await response.json();
+  console.log(da);
+
+  return guestComments;
+}
+
+//RETRIEVING & CALLING DATA
+
+function getGuestComments() {
+  const retrievedData = localStorage.getItem("clickerData");
+  if (retrievedData) {
+    const loadedData = JSON.parse(retrievedData);
+    clickerData.cookieAmount = loadedData.cookieAmount;
+    clickerData.cps = loadedData.cps;
+  }
+}
+
+loadedData();
+
+//CREATING UPGRADES Comments
+
+const addGuestComments = document.getElementById("guestFeedback");
+
+async function showComments() {
+  const comments = await cookieUpgrades();
+
+
+  upgrades.forEach((upgrades, i) => {
+    //
+    const commentsBox = document.createElement("Comment");
+    commentsBox.textContent = `${Name} (Name: ${upgrades.cost}, +${upgrades.increase} CPS)`;
+    console.log(upgradesButton);
+
+    shopContainer.appendChild(upgradesButton);
+
+
+createUpgrades();
+
+
+//Cookie Clicker Code
+
+ const stringifiedClickerData = JSON.stringify(clickerData);
+  localStorage.setItem("clickerData", stringifiedClickerData);
+}, 1000);
+
+//CALLING API
+
+async function cookieUpgrades() {
+  const response = await fetch(
+    "https://cookie-upgrade-api.vercel.app/api/upgrades"
+  );
+
+  console.log(response);
+
+  const data = await response.json();
+  console.log(data);
+
+  return data;
+}
+cookieUpgrades();
+
+//RETRIEVING & CALLING DATA
+
+function purchaseUpdgrades(i) {
+  upgrades = null;
+}
+
+function loadedData() {
+  const retrievedData = localStorage.getItem("clickerData");
+  if (retrievedData) {
+    const loadedData = JSON.parse(retrievedData);
+    clickerData.cookieAmount = loadedData.cookieAmount;
+    clickerData.cps = loadedData.cps;
+  }
+}
+
+loadedData();
+
+//CREATING UPGRADES BUTTONS
+
+const returnedData = document.getElementById("shop-container");
+
+async function createUpgrades() {
+  const upgrades = await cookieUpgrades();
+
+  //my original code...(double check with Manny or Joe)
+  //for (let i = 0; i < upgrades.length; i++) {
+
+  //ChatGPT suggestion *double check understanding*
+
+  upgrades.forEach((upgrades, i) => {
+    
+    const upgradesButton = document.createElement("button");
+    upgradesButton.textContent = `${upgrades.name} (Cost: ${upgrades.cost}, +${upgrades.increase} CPS)`;
+    console.log(upgradesButton);
+
+    shopContainer.appendChild(upgradesButton);
+  )}};
+createUpgrades();
+
 
 //TODO: CREATE (POST) new data in the database
 app.post("/add-staff", (req, res) => {
--- server/server.js
+++ server/server.js
@@ -11,9 +11,9 @@ app.listen(PORT, function () {
   console.log(`Server is running in port ${PORT}`);
 });
 
-app.get("/", function (req, res) {
-  res.json({ message: "Welcome to the server. GET comfy" });
-});
+// app.get("/", function (req, res) {
+//   res.json({ message: "Welcome to the server. GET comfy" });
+// });
 
 //imports
 //initialise express and cors
@@ -23,4 +23,87 @@ app.get("/", function (req, res) {
 
 //TODO: I want to READ all the data from my table
 
+// http://localhost:8080/staff --> endpoint, params
+app.get("/getDBData", async function (req, res) {
+  //we need to query our database here
+  const query = await db.query(`SELECT * FROM userFeedback;`);
+  console.log(query);
+  //parse data into JSON and wrangle data
+  res.json(query.rows);
+});
+
+//find wrangled data demo
+
+//TODO: I want to READ all the data from the staff table
+// http://localhost:8080/staff --> endpoint, params
+app.get("/getDBData", async function (req, res) {
+  //we need to query our database here
+  const query = await db.query(`SELECT * FROM guestFeedback;`);
+  console.log(query);
+  //parse data into JSON and wrangle data
+  res.json(query.rows);
+});
+
+//Wrangle Data
+async function retrieveData() {
+  const response = await fetch("http://localhost:8080/staff ");
+  const data = await response.json();
+  console.log(retrieveData);
+  //filtering the data to the properties that we need --> data wrangling
+  const wrangledData = retrieveData.text;
+  console.log(wrangledData);
+  return wrangledData;
+}
+
+//TODO: Render the data on the DOM
+function addGuestInfo(localhost) {
+  const guestFeedback = document.getElementById("guestFeedback");
+  //update the values of src and alt to match the foxes data
+  guestFeedback.textContent = localhost;
+  guestFeedback.appendChild(guestFeedback);
+}
+
+//TODO: put the data in the API
+async function addGuestFeedbacktoAPI() {
+  const addGuestInfo = await guestFeedback();
+  addGuestInfo(guestFeedback);
+}
+
+addGuestFeedbacktoAPI();
+
 //TODO: I want to CREATE new data in the table
+
+
+async function retrieveComments() {
+  const response = await fetch(
+    "https://localhost:8080/guests"
+  );
+
+  console.log(response);
+
+  const guestsComments = await response.json();
+  console.log(da);
+
+  return guestComments;
+}
+
+//RETRIEVING & CALLING DATA
+
+//CREATING UPGRADES Comments
+
+const addGuestComments = document.getElementById("guestFeedback");
+
+async function showComments() {
+  const comments = await cookieUpgrades();
+
+  upgrades.forEach((upgrades, i) => {
+    //
+    const commentsBox = document.createElement("p");
+    commentsBox.textContent = `${Name} (Name: ${upgrades.cost}, +${upgrades.increase} CPS)`;
+    console.log(upgradesButton);
+
+    shopContainer.appendChild(upgradesButton);
+
+}});
+
+createUpgrades();
